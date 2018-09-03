884ecfdb5a480a36c3011f3f51e31ac5f6add6f7
Matt McClean <mmcclean@amazon.com>
2018-09-03 22:22:37 +0000

    updated code to load weights and update notebook

--- container/lesson1/lesson1.py
+++ container/lesson1/lesson1.py
@@ -105,7 +105,7 @@ def model_fn(model_dir):
     model = learn.model
 
     with open(os.path.join(model_dir, 'model.pth'), 'rb') as f:
-        model.load_state_dict(torch.load(f))
+        model.load_state_dict(torch.load(f, map_location=lambda storage, loc: storage))
     logger.debug('Returning model')
     model.cpu()
     return model.eval()
